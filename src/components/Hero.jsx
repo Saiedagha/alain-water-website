0f1a66256e3e586041c79b64e1543c584ce56052
saeed agha <saidagha1310@gmail.com>
2026-07-18 17:57:06 +0300

    ؤ

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,7 +49,6 @@ export default function Hero() {
           ref={mobileRailRef}
           onScroll={onMobileScroll}
           className="hero-scroller flex w-full overflow-x-auto md:hidden"
-          style={{ touchAction: 'pan-x' }}
           aria-label="Hero banners"
         >
           {HERO_SLIDES.map((slide) => (
